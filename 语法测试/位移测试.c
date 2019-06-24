ec17522f048bab006c376599cb54eab460de12a1
imba-tjd <109224573@qq.com>
2019-06-24 22:10:50 +0800

    Create 位移测试.c

---
+++ 语法测试/位移测试.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define PR(x) printf("%d\n", x)
+
+int main(void)
+{
+    PR(INT_MAX); // 2147483647
+
+    PR(0xffff);      // 65535
+    PR(0xffff >> 1); // 32767
+
+    PR(~0);       // -1
+    PR(~0U);      // -1
+    PR(~0U >> 1); // 2147483647
+
+    PR(0x7fff);                      // 32767
+    PR((unsigned short)0xffff >> 1); // 32767
+    PR((short)0xffff >> 1);          // -1
+}
