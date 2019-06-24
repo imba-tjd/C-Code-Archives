1dd205569ee068d85584446c1e7f7a63365076bd
imba-tjd <109224573@qq.com>
2019-06-24 19:11:47 +0800

    Create 数组越界.c

---
+++ 数组越界.c
@@ -0,0 +1,15 @@
+// 不开优化时，结果为11，因为分布上a[4]就是count；开了以后为3，因为count在循环内只有一种改变的可能，就是循环的次数
+#include <stdio.h>
+int main(void)
+{
+    int b = 0;
+    int count = 0;
+    int a[3];
+    while (b++ < 3)
+    {
+        a[b] = 10;
+        count++;
+    }
+    printf("%d", count);
+    return 0;
+}
