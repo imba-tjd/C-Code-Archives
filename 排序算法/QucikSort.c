64f00c8090c9a244dcf5e5527dbde8cd9992486b
imba-tjd <109224573@qq.com>
2019-06-26 14:46:37 +0800

    Update QucikSort.c

--- 排序算法/QucikSort.c
+++ 排序算法/QucikSort.c
@@ -1,114 +1,67 @@
+// 这几个算法都是原地的算法；非原地的有许多不同，但本身的思路特别简单
+// 这几个算法都是递归的算法；改成迭代的需要把间隔从大往小处理，底下的返回pivot的位置就好
+// 因为递归调用时会传指针进去，所以不能加restrict？
 // 待看：https://www.zhihu.com/question/39214230
 #include <assert.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // malloc
 #include <string.h> // memcpy
 
-typedef enum SortType {
-    SmallToBig = 1,
-    BigToSmall = -1
-} SortType;
-
-void QuickSort1(void *array, int count, size_t size, int (*Compare)(const void *a, const void *b), SortType type);
-void QuickSort2(int *array, int length);
-void QuickSort3(int array[], int left, int right);
-void Swap(void *a, void *b, size_t size);
-int Compare(const void *a, const void *b);
-
-// 排序算法思想来自http://developer.51cto.com/art/201403/430986.htm，实现独立。
-void QuickSort1(void *array, int count, size_t size, int (*Compare)(const void *a, const void *b), SortType type)
-{
-    assert(array != NULL);
-    if (count <= 1)
-        return;
-
-    // count > 1, right > left
-    int left = 0, right = count - 1; //  如果left = 1，当count == 2时，无法判断（或需要额外判断）这两项是否须要交换
-    while (left != right)            // 不会出现 left > right 的情况
-    {
-        // 虽然sizeof(void*) == 8，但void*类型加1以后就只会加一，不会乘以8。所以不能直接指针+间隔，要用间隔乘以大小再加。
-        if (Compare(array, array + right * size) * type < 0) // 从小到大排序时，若右边的大于枢轴
-        {
-            right--;  // 每次只走一步
-            continue; // 不用交换，继续移动
-        }
-        if (Compare(array, array + left * size) * type > 0) // 从小到大排序时，若左边的小于枢轴
-        {
-            left++;
-            continue;
-        }
-
-        Swap(array + left * size, array + right * size, size);
-    }
-    Swap(array, array + right * size, size); // left == right
-
-    QuickSort1(array, left * size, size, Compare, type);
-    QuickSort1(array + (left + 1) * size, count - (left + 1), size, Compare, type);
-}
-
-void Swap(void *a, void *b, size_t size)
-{
-    void *temp = malloc(size);
-    temp = memcpy(temp, a, size);
-    memcpy(a, b, size);
-    memcpy(b, temp, size);
-    free(temp);
-}
-
-int Compare(const void *a, const void *b)
-{
-    int *c = (int *)a;
-    int *d = (int *)b;
-    return *c >= *d ? *c > *d ? 1 : 0 : -1;
-}
-
-void QuickSort2(int *array, int length) // 书上的算法
+// ----------------------------------
+// 严书上的算法；有一点点不同，没有空a[0]出来
+void QuickSort2(int array[], size_t length)
 {
     assert(array != NULL);
 
-    int left = 0, right = length - 1;
-    int temp = *array;
+    int pivot = *array; // 选a[0]作为枢轴记录下来，则这个位置就可以被覆盖了；如果要选中间的，保存选好的后把a[0]赋给那个位置
 
-    if (left >= right)
+    int left = 0, right = length - 1; // 闭区间
+    if (left >= right)                // length为0或1
         return;
 
     while (left < right)
     {
-        while (left < right && array[right] >= temp)
+        while (left < right && array[right] >= pivot) // 从右往左找一个小于枢轴的
             right--;
-        array[left] = array[right];
+        array[left] = array[right]; // 赋值后right位置可以被覆盖
 
-        while (left < right && array[left] <= temp)
+        while (left < right && array[left] <= pivot) // 从左往右找一个大于枢轴的
             left++;
         array[right] = array[left];
     }
-    array[left] = temp;
+    assert(left == right); // 必定left == right，因为子循环里只会走一步且完成了赋值
+    array[left] = pivot;   // 枢轴的最终位置
 
     QuickSort2(array, left);
     QuickSort2(array + left + 1, length - (left + 1));
 }
 
-void QuickSort3(int array[], int left, int right) // wiki上的C#算法。为了和其他例子保持一致，right不使用非对称边界
+// ----------------------------------
+// wiki上的C#算法，用的是闭区间
+// 不把枢轴单独拿出来，而是和其他数一起排列
+// 左边找一个大的，右边找一个小的，交换，但如果左边找到了，右边没找到怎么办？
+// 还有不要试图交换后left++;right++，有可能交错，导致状态变得各种复杂
+// 总之看不懂太原理，试图把那个单独的分号改掉，失败，过不了有重复数据的测试
+void QuickSort3(int array[const], int left, int right)
 {
     assert(array != NULL);
 
     if (left < right)
     {
-        int middle = array[(left + right) / 2]; // 随便选一个作为枢轴
-        int i = left, j = right;
-        int t;
-        while (1) // 不把枢轴单独拿出来，而是和其他数一起排列
+        int middle = array[left + (right - left) / 2]; // 选一个中间的数作为枢轴；注意防越界
+        int i = left - 1, j = right + 1;
+        while (1)
         {
-            while (array[i] < middle)
-                i++;
+            while (array[++i] < middle) // 与其它的不同，先走左边
+                ;
 
-            while (array[j] > middle)
-                j--;
+            while (array[--j] > middle)
+                ;
 
             if (i >= j)
                 break;
 
-            t = array[j];
+            int t = array[j];
             array[j] = array[i];
             array[i] = t;
         }
@@ -118,27 +71,89 @@ void QuickSort3(int array[], int left, int right) // wiki上的C#算法。为了
     }
 }
 
-#define LENGTH 10
-#define A
+// ----------------------------------
+// 算法导论的算法。快慢指针从左开始，[0,slow)是小于枢轴的部分，[slow,fast)是大于枢轴的部分
+// fast是当前评估的位置，如果大于枢轴就继续往右走；如果小于，则于slow交换
+void QuickSort4(int array[const], size_t len)
+{
+    if (len <= 1)
+        return;
+
+    int pivot = array[len - 1]; // 选最后一个数为枢轴
+    int slow = 0;
+    for (int fast = 0; fast < len; fast++)
+    {
+        if (array[fast] <= pivot) // pivot算作左边的部分
+        {
+            int t = array[fast];
+            array[fast] = array[slow];
+            array[slow] = t;
+            slow++;
+        }
+    }
+    QuickSort4(array, slow - 1);          // 最后交换pivot后slow还++了，所以slow-1才是pivot的位置；且这个参数是len，即开区间，所以直接传pivot的位置就好
+    QuickSort4(array + slow, len - slow); //slow处即pivot的位置，但
+}
+
+// ----------------------------------
+// 测试
+void PrintArr(const int arr[restrict], size_t len)
+{
+    for (size_t i = 0; i < len; i++)
+        printf("%d ", arr[i]);
+    putchar('\n');
+}
+
+int *GetArr(int *len)
+{
+    // int array[] = {76, 32, 71, 52, 65, 69, 2, 27, 30, 67};
+    int array[] = {85, 75, 75, 59, 7, 30, 36, 16, 27, 80, 53, 78, 69, 85, 75};
+    *len = sizeof(array) / sizeof(*array);
+
+    int siz = *len * sizeof(int);
+    int *arr = malloc(siz);
+    memcpy(arr, array, siz);
+    return arr;
+}
+
+void Test2()
+{
+    int len;
+    int *arr = GetArr(&len);
+
+    QuickSort2(arr, len);
+    PrintArr(arr, len);
+
+    free(arr);
+}
+
+void Test3()
+{
+    int len;
+    int *arr = GetArr(&len);
+
+    QuickSort3(arr, 0, len - 1);
+    PrintArr(arr, len);
+
+    free(arr);
+}
+
+void Test4()
+{
+    int len;
+    int *arr = GetArr(&len);
+
+    QuickSort4(arr, len);
+    PrintArr(arr, len);
+
+    free(arr);
+}
 
 int main(void)
 {
-    int array[LENGTH] = {76, 32, 71, 52, 65, 69, 2, 27, 30, 67};
-
-#ifdef A
-    QuickSort1(array, LENGTH, sizeof(int), &Compare, SmallToBig);
-#endif
-
-#ifdef B
-    QuickSort2(array, LENGTH);
-#endif
-
-#ifdef C
-    QuickSort3(array, 0, LENGTH - 1);
-#endif
-
-    for (int i = 0; i < LENGTH; i++)
-        printf("%d\n", array[i]);
+    Test2();
+    Test3();
+    Test4();
 
     return 0;
 }
