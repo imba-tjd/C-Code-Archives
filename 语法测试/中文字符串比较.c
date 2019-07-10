73a4524f2c12856511afcda45d856754fb6e6a85
imba-tjd <109224573@qq.com>
2019-07-10 08:33:54 +0800

    Update 中文字符串比较.c

--- 语法测试/中文字符串比较.c
+++ 语法测试/中文字符串比较.c
@@ -11,22 +11,21 @@ int main(void)
 {
     setlocale(LC_ALL, "chs");
 
-    printf("%d\n", strcmp("赵六", "王五")); // 1
-    printf("%d\n", strcmp("王五", "李四")); // 1
-    printf("%d\n", strcmp("李四", "张三")); // 1
-    putchar('\n');
+    // 因为我的代码用的是UTF8编码，这部分的硬编码字符串也是，此时strcmp无意义；而手动输入是能正常比较的
+    // printf("%d\n", strcmp("赵六", "王五")); // 1
+    // printf("%d\n", strcmp("王五", "李四")); // 1
+    // printf("%d\n", strcmp("李四", "张三")); // 1
+
+    // 输入赵六 王五 李四 张三：
+    normalCmp(); // 李四 王五 张三 赵六，符合拼音顺序
+    // putchar('\n');
 
     wprintf(L"%d\n", wcscmp(L"赵六", L"王五")); // 1
     wprintf(L"%d\n", wcscmp(L"王五", L"李四")); // 1
     wprintf(L"%d\n", wcscmp(L"李四", L"张三")); // 1
     putchar('\n');
 
-    // 所以输入赵六 王五 李四 张三，输出顺序应该不会变
-    // 可实际上，Win下非宽字符输出顺序，为李四 王五 张三 赵六；宽字符输出顺序为张三 李四 王五 赵六，都变了
-    // 我猜测与输入有关，即输入和编译进去的字符串不一样。
-
-    normalCmp();
-    wideCmp();
+    // wideCmp(); // 张三 李四 王五 赵六
 
     return 0;
 }
@@ -36,7 +35,6 @@ int main(void)
 Win下的vscode的debug的watch中加一个 `strcmp("你好","你好")` 或者 `strcmp("123","456")` 会直接导致gdb崩溃退出；Linux下有效。
 Linux下那六个直接比较，只会输出三个1，而注释掉任意一组也还是三个1；normalCmp的结果是 张三 李四 王五 赵六，wideCmp没有输出！
 曾经还出现过clang和gcc结果不同、`strcmp("李四", "张三")` 为1但 `strcmp("张三"，"李四")` 不为-1的情况
-总之，我暂时的结论就是，不要在C语言里写中文！
 */
 
 #define MAXSIZE 20
@@ -44,7 +42,7 @@ Linux下那六个直接比较，只会输出三个1，而注释掉任意一组�
 
 void normalCmp(void)
 {
-    printf("not wide\n");
+    printf("Not Wide: ");
 
     char sn[N][MAXSIZE], t[MAXSIZE];
     for (int i = 0; i < N; i++)
@@ -54,7 +52,7 @@ void normalCmp(void)
     {
         int k = i; // 选择排序
         for (int j = i + 1; j < N; j++)
-            if (strcmp(sn[j], sn[k]) < 0) // 从大到小排序
+            if (strcmp(sn[j], sn[k]) < 0) // 从大到小排序，但结果确是拼音从前往后排的
                 k = j;
 
         strcpy(t, sn[i]);
@@ -62,14 +60,15 @@ void normalCmp(void)
         strcpy(sn[k], t);
     }
 
-    printf("\n");
+    putchar('\n');
     for (int i = 0; i < N; i++)
-        printf("%s\n", sn[i]);
+        printf("%s ", sn[i]);
+    putchar('\n');
 }
 
 void wideCmp(void)
 {
-    wprintf(L"wide\n");
+    wprintf(L"Wide: ");
 
     wchar_t sn[N][MAXSIZE], t[MAXSIZE];
     for (int i = 0; i < N; i++)
@@ -87,7 +86,8 @@ void wideCmp(void)
         wcscpy(sn[k], t);
     }
 
-    printf("\n");
+    putchar('\n');
     for (int i = 0; i < N; i++)
-        wprintf(L"%s\n", sn[i]);
+        wprintf(L"%s ", sn[i]);
+    putchar('\n');
 }
