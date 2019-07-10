e1ab53dc64a6f426064ff50872417e248ecabc02
imba-tjd <109224573@qq.com>
2019-07-10 14:54:16 +0800

    fix clang compatibility

--- 语法测试/数组越界.c
+++ 语法测试/数组越界.c
@@ -1,5 +1,7 @@
 // 不开优化时，结果为11，因为分布上a[4]就是count；开了以后为3，因为count在循环内只有一种改变的可能，就是循环的次数
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
+#endif
 #include <stdio.h>
 int main(void)
 {
