d153a9305e332fad85c79dcc23f511a47a02cc46
imba-tjd <109224573@qq.com>
2019-10-15 21:45:12 +0800

    Create typeof.h

---
+++ 语法测试/typeof.h
@@ -0,0 +1,12 @@
+// https://segmentfault.com/q/1010000020605575
+#define typeof(x) _Generic((x),                                                 \
+        _Bool: "_Bool",                  unsigned char: "unsigned char",          \
+         char: "char",                     signed char: "signed char",            \
+    short int: "short int",         unsigned short int: "unsigned short int",     \
+          int: "int",                     unsigned int: "unsigned int",           \
+     long int: "long int",           unsigned long int: "unsigned long int",      \
+long long int: "long long int", unsigned long long int: "unsigned long long int", \
+        float: "float",                         double: "double",                 \
+  long double: "long double",                   char *: "pointer to char",        \
+       void *: "pointer to void",                int *: "pointer to int",         \
+      default: "other")
